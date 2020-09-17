f87149045304472f70a718df03fbca652e681674
jiachengli1999 <35944405+jiachengli1999@users.noreply.github.com>
2020-09-17 19:48:52 -0400

    Updates

---
+++ precache-manifest.006f8b46c611327cf11f32a3a5f1bb0c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446a51de63a93bf2d47847a13d8d8b69",
+    "url": "/IDM-Fall-2020-Web-dev/index.html"
+  },
+  {
+    "revision": "a2ee24385bb33bcbc3b2",
+    "url": "/IDM-Fall-2020-Web-dev/static/css/main.459f9471.chunk.css"
+  },
+  {
+    "revision": "495abb249a0d4a96f0a1",
+    "url": "/IDM-Fall-2020-Web-dev/static/js/2.7329132c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/IDM-Fall-2020-Web-dev/static/js/2.7329132c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ee24385bb33bcbc3b2",
+    "url": "/IDM-Fall-2020-Web-dev/static/js/main.6746d0da.chunk.js"
+  },
+  {
+    "revision": "5bb65006aab5ded59b6f",
+    "url": "/IDM-Fall-2020-Web-dev/static/js/runtime-main.b37d9002.js"
+  },
+  {
+    "revision": "29f05e7bc8908da408e1fb42cd84546e",
+    "url": "/IDM-Fall-2020-Web-dev/static/media/blue.29f05e7b.PNG"
+  },
+  {
+    "revision": "a7e4d25dcc69cc883934a8154ee6f66a",
+    "url": "/IDM-Fall-2020-Web-dev/static/media/fake_review.a7e4d25d.PNG"
+  },
+  {
+    "revision": "2cec37a401f46571888474a6b9e3635b",
+    "url": "/IDM-Fall-2020-Web-dev/static/media/floor.2cec37a4.PNG"
+  },
+  {
+    "revision": "e6de42a529576018c19caccb411b6a66",
+    "url": "/IDM-Fall-2020-Web-dev/static/media/meme_mona2.e6de42a5.PNG"
+  },
+  {
+    "revision": "d83967f87108eccfb00b2b0ccc91cbdf",
+    "url": "/IDM-Fall-2020-Web-dev/static/media/mona.d83967f8.PNG"
+  },
+  {
+    "revision": "777a0725f6cf635b61aa12d3ec560ecb",
+    "url": "/IDM-Fall-2020-Web-dev/static/media/mona_pic.777a0725.PNG"
+  },
+  {
+    "revision": "307b0623ef684ad319fef4a4f005f69f",
+    "url": "/IDM-Fall-2020-Web-dev/static/media/rating.307b0623.PNG"
+  },
+  {
+    "revision": "8b40468a1fa6a5009a85c242c064da48",
+    "url": "/IDM-Fall-2020-Web-dev/static/media/scene.8b40468a.PNG"
+  },
+  {
+    "revision": "0f0f8dc796721135977661e98b51d198",
+    "url": "/IDM-Fall-2020-Web-dev/static/media/sunflowers.0f0f8dc7.png"
+  },
+  {
+    "revision": "3e17d82549079a6d5cf73c1af131c296",
+    "url": "/IDM-Fall-2020-Web-dev/static/media/wiki.3e17d825.PNG"
+  }
+]);
